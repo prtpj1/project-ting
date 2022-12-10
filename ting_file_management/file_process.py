5c3f6173b05d9cc913f0172256f049b2dac6120c
prtpj1 <prtpj1@gmail.com>
2022-12-10 20:49:01 -0300

    :yum: feat:(Requisite 03) function process :arrow_right: the function convert the list content from txt_importer into a dict inside a queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,25 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_text = txt_importer(path_file)
+
+    for content in instance._data:
+        if path_file in content["nome_do_arquivo"]:
+            return None
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_text),
+        "linhas_do_arquivo": file_text,
+    }
+    instance.enqueue(file_info)
+    sys.stdout.write(str(file_info))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    ...
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    ...
