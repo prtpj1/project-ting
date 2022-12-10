c57e72b4d8d6aead3ef1afb6e5f9b1d3bd90617e
prtpj1 <prtpj1@gmail.com>
2022-12-10 18:44:43 -0300

    :yum: feat:(Requisite 02) function txt_importer :arrow_right: the function import news from a file txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    ...
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, "r") as file:
+                content = file.read()
+                new_content = content.split("\n")
+                return new_content
+        except FileNotFoundError:
+            return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    return sys.stderr.write("Formato inválido\n")
