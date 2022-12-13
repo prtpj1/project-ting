b5ef5c3b77d0911f0c741bf51b3e2a78a9d3bafb
prtpj1 <prtpj1@gmail.com>
2022-12-12 23:03:20 -0300

    :yum: feat:(Requisite 06) function exists_word :arrow_right: the function verify if a parameter word exists in all processed files

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    existent_word = list()
+    occurrences = list()
+    data = instance._data[0]
+
+    for pos, line in enumerate(data["linhas_do_arquivo"]):
+        if word.casefold() in line.casefold():
+            occurrences.append({"linha": pos + 1})
+
+    if len(occurrences) > 0:
+        existent_word.append(
+            {
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": occurrences,
+            }
+        )
+
+    return existent_word
 
 
 def search_by_word(word, instance):
