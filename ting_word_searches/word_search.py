e5e5d35ef5e5ff93dc288549748294087cc3f1e5
prtpj1 <prtpj1@gmail.com>
2024-09-26 22:59:05 -0300

    :yum: feat: adds interactive word search in file with user inputs :arrow_right: Implemented user inputs for searching a word within a specified file :arrow_right: Added input prompts for the word to search and the file path :arrow_right: Integrated the Queue and process functions to handle file processing and word searching :arrow_right: Formatted the output in JSON for better readability

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,22 +1,38 @@
+import json
+from ting_file_management.queue import Queue
+from ting_file_management.file_process import process
+
+
 def exists_word(word, instance):
     existent_word = list()
     occurrences = list()
     data = instance._data[0]
 
-    for pos, line in enumerate(data["linhas_do_arquivo"]):
+    for pos, line in enumerate(data["file_lines"]):
         if word.casefold() in line.casefold():
-            occurrences.append({"linha": pos + 1})
+            occurrences.append({"line": pos + 1})
 
     if len(occurrences) > 0:
         existent_word.append(
             {
-                "palavra": word,
-                "arquivo": data["nome_do_arquivo"],
-                "ocorrencias": occurrences,
+                "word": word,
+                "file": data["file_name"],
+                "occurrences": occurrences,
             }
         )
 
     return existent_word
+
+
+if __name__ == "__main__":
+    word = input("Type the word to be searched: ")
+    file_path = input("Type the file path where the word will be searched: ")
+
+    queue_instance = Queue()
+    process(file_path, queue_instance)
+    result = exists_word(word, queue_instance)
+
+    print(json.dumps(result, indent=2))
 
 
 def search_by_word(word, instance):
